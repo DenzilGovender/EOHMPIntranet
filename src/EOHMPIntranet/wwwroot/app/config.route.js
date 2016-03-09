51692490ac121e32117557d9cf04b45adda868e1
DenzilGovender <denzil_govender@hotmail.com>
2016-03-09 11:44:38 +0200

    added solution

---
+++ src/EOHMPIntranet/wwwroot/app/config.route.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    var routeProvider = function ($routeProvider, $locationProvider) {
+
+        var viewBase = '/app/components/';
+
+        $routeProvider.when('/index', {
+            controller: 'IndexController',
+            templateUrl: 'index.html',
+            controllerAs: 'vm'            
+        }).otherwise({ redirectTo: '/' });
+    }
+
+    angular.module('EOHIntranet').config(['$routeProvider', '$locationProvider', routeProvider]);
+    routeProvider.$inject = ['$routeProvider', '$locationProvider'];   
+   
+})();
---
+++ src/EOHMPIntranet/wwwroot/indexController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';   
+
+    function indexController($location, $scope, $firebaseObject) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'indexController';
+       var ref = new Firebase("https://flickering-torch-5362.firebaseio.com");
+
+
+        $scope.navigateTo = function (url) {
+            $location.path(url);
+        }
+
+    }
+
+    angular.module('EOHIntranet').controller('indexController', indexController);
+    indexController.$inject = ['$location', '$scope','$firebaseObject'];
+})();
