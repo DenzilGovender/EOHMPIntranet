dc183e48ba0cdb784be61ee0319e926d7e379f73
JoelKap <joelkapuku@hotmail.com>
2016-03-11 00:28:23 +0200

    alert

---
+++ src/EOHMPIntranet/wwwroot/app/alert/alertDialog.service.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+    function alertDialogService($http, $q) {
+        var self = this;
+        self.path = [];
+        self.header = undefined;
+        self.message = undefined;
+       
+
+        this.getPath = function()
+        {
+            return this.path;
+        }
+        this.getMessage = function () {
+            return this.message;
+        }
+        this.getHeader = function () {
+            return this.header;
+        }
+
+        this.setPath = function(path)
+        {
+            this.path = path;
+        }
+
+        this.setHeaderAndMessage = function (header, message) {
+            self.header = header;
+            self.message = message;
+        }
+
+    }
+
+    angular.module('GPOApp').service('alertDialogService', alertDialogService);
+    alertDialogService.$inject = ['$http', '$q'];
+})();
