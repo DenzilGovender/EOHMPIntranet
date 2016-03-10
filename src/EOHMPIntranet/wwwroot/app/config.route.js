1a3ea5f96d57a1e89ba6deab21d297df41ec765f
HBadenhorst <henk.badenhorst@outlook.com>
2016-03-11 00:33:13 +0200

    Dashboard UI

--- src/EOHMPIntranet/wwwroot/app/config.route.js
+++ src/EOHMPIntranet/wwwroot/app/config.route.js
@@ -9,79 +9,98 @@
             templateUrl: 'index.html',
             controllerAs: 'vm'            
         })
-          .when('/AdminRecruitment', {
-              controller: 'AdmimRecruitmentCreateController',
-              templateUrl: viewBase + '/recruitment/adminRecruitmentCreate.html',
-              controllerAs: 'vm'
-          })
-            .when('/ViewEmployee', {
-                controller: 'ViewEmployeeController',
-                templateUrl: viewBase + '/employee/viewEmployee/viewEmployee.html',
-                controllerAs: 'vm'
-            }).when('/SearchEmployee', {
-                            controller: 'SearchEmployeeController',
-                            templateUrl: viewBase + '/employee/searchEmployee/searchEmployee.html',
-                            controllerAs: 'vm'
-                        })
 
-                                    .when('/CreateEmployee', {
-                                        controller: 'EmployeeCreateController',
-                                        templateUrl: viewBase + '/employee/createEmployee/employeeCreate.html',
-                                        controllerAs: 'vm'
-                                    })
+        .when('/dashboard', {
+            controller: 'dashboardController',
+            templateUrl: viewBase + '/dashboard/dashboard.html',
+            controllerAs: 'vm'
+        })
 
-             .when('/ViewRecruitment', {
-                 controller: 'RecruitmentViewController',
-                 templateUrl: viewBase + '/recruitment/viewRecruitment.html',
-                 controllerAs: 'vm'
-             })
+        .when('/AdminRecruitment', {
+            controller: 'AdmimRecruitmentCreateController',
+            templateUrl: viewBase + '/recruitment/adminRecruitmentCreate.html',
+            controllerAs: 'vm'
+        })
 
-             .when('/RecruitmentDetail', {
-                 controller: 'RecruitmentDetailController',
-                 templateUrl: viewBase + '/recruitment/recruitment.detail.html',
-                 controllerAs: 'vm'
-             })
+        .when('/ViewEmployee', {
+            controller: 'ViewEmployeeController',
+            templateUrl: viewBase + '/employee/viewEmployee/viewEmployee.html',
+            controllerAs: 'vm'
+        })
 
-             .when('/login', {
-                 controller: 'LoginController',
-                 templateUrl: viewBase + '/login/login.html',
-                 controllerAs: 'vm'
-             })
+        .when('/SearchEmployee', {
+            controller: 'SearchEmployeeController',
+            templateUrl: viewBase + '/employee/searchEmployee/searchEmployee.html',
+            controllerAs: 'vm'
+        })
 
-            .when('/Employee', {
-                          controller: 'AdmimRecruitmentCreateController',
-                          templateUrl: viewBase + '/recruitment/adminRecruitmentCreate.html',
-                          controllerAs: 'vm'
-                                    })
+        .when('/CreateEmployee', {
+            controller: 'EmployeeCreateController',
+            templateUrl: viewBase + '/employee/createEmployee/employeeCreate.html',
+            controllerAs: 'vm'
+        })
 
-            .when('/CreateEvent', {
-                controller: 'CreateEventController',
-                templateUrl: viewBase + '/event/create/createEvent.html',
-                controllerAs: 'vm'
-            })
-            .when('/Events', {
-                controller: 'ListEventController',
-                templateUrl: viewBase + '/event/list/listEvent.html',
-                controllerAs: 'vm'
-            })
-            .when('/ViewEvent', {
-                controller: 'ViewEventController',
-                templateUrl: viewBase + '/event/view/viewEvent.html',
-                controllerAs: 'vm'
-            })
-          .when('/CreateProject', {
-              controller: 'ProjectCreateController',
-              templateUrl: viewBase + '/project/project.create.html',
-              controllerAs: 'vm'
-          }).when('/ProjectsView', {
-              controller: 'ProjectViewController',
-              templateUrl: viewBase + '/project/project.view.html',
-              controllerAs: 'vm'
-          }).when('/ProjectDetail', {
-              controller: 'ProjectDetailController',
-              templateUrl: viewBase + '/project/project.detail.html',
-              controllerAs: 'vm'
-          }).otherwise({ redirectTo: '/' });
+        .when('/ViewRecruitment', {
+            controller: 'RecruitmentViewController',
+            templateUrl: viewBase + '/recruitment/viewRecruitment.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/RecruitmentDetail', {
+            controller: 'RecruitmentDetailController',
+            templateUrl: viewBase + '/recruitment/recruitment.detail.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: viewBase + '/login/login.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/Employee', {
+            controller: 'AdmimRecruitmentCreateController',
+            templateUrl: viewBase + '/recruitment/adminRecruitmentCreate.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/CreateEvent', {
+            controller: 'CreateEventController',
+            templateUrl: viewBase + '/event/create/createEvent.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/Events', {
+            controller: 'ListEventController',
+            templateUrl: viewBase + '/event/list/listEvent.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/ViewEvent', {
+            controller: 'ViewEventController',
+            templateUrl: viewBase + '/event/view/viewEvent.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/CreateProject', {
+            controller: 'ProjectCreateController',
+            templateUrl: viewBase + '/project/project.create.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/ProjectsView', {
+            controller: 'ProjectViewController',
+            templateUrl: viewBase + '/project/project.view.html',
+            controllerAs: 'vm'
+        })
+
+        .when('/ProjectDetail', {
+            controller: 'ProjectDetailController',
+            templateUrl: viewBase + '/project/project.detail.html',
+            controllerAs: 'vm'
+        })
+
+        .otherwise({ redirectTo: '/' });
     }
 
     angular.module('EOHIntranet').config(['$routeProvider', '$locationProvider', routeProvider]);
