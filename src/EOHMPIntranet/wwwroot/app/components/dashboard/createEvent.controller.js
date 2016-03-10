be14c03280557485565f5abc3271af3d1bde8f8a
HBadenhorst <henk.badenhorst@outlook.com>
2016-03-10 22:39:53 +0200

    Wrapper UI

---
+++ src/EOHMPIntranet/wwwroot/app/components/dashboard/createEvent.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+
+
+    function CreateEventController($location, $firebaseArray, $mdToast) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'Create Event';
+        var ref = new Firebase("https://flickering-torch-5362.firebaseio.com/Event");
+        vm.minDate = new Date();
+
+        vm.save = function (event)
+        {
+            var AddEvent = $firebaseArray(ref);
+            var date = event.date;
+            event.date = event.date.toString();
+            AddEvent.$add(event);
+            $mdToast.show(
+                          $mdToast.simple()
+                          .textContent('Event is saved successfully!')
+                          .hideDelay(3000));
+            $location.path('/Events');
+        }
+
+        vm.cancel = function () {
+            $location.path('/Events');
+        }
+    }
+
+    angular.module('EOHIntranet').controller('CreateEventController', CreateEventController);
+    CreateEventController.$inject = ['$location', '$firebaseArray', '$mdToast'];
+
+})();
