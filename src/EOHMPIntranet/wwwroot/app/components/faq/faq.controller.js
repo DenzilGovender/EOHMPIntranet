556fdabf394bf807c8e7c02e20fc2bcc19a6459b
HBadenhorst <henk.badenhorst@outlook.com>
2016-03-11 14:39:35 +0200

    Video FAQ page

---
+++ src/EOHMPIntranet/wwwroot/app/components/faq/faq.controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+
+
+    function faqController($location, $sessionStorage, $firebaseObject, $firebaseArray, EventService, $mdToast, modal) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'Faq';
+        vm.events = [];
+        
+
+        var ref = new Firebase("https://eoh-intranet.firebaseio.com");
+
+        init();
+
+        function init() {
+
+         
+        }
+
+       
+    }
+
+    angular.module('EOHIntranet').controller('faqController', faqController);
+    faqController.$inject = ['$location', '$sessionStorage', '$firebaseObject', '$firebaseArray', 'EventService', '$mdToast', 'modal'];
+})();
