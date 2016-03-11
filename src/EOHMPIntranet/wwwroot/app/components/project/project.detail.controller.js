087ad0162dae42852e101d3213c7bb5aaa8ec5ce
Zitha <Ndavhe@tussotechnologies.co.za>
2016-03-11 05:25:02 +0200

    Stayling changes

--- src/EOHMPIntranet/wwwroot/app/components/project/project.detail.controller.js
+++ src/EOHMPIntranet/wwwroot/app/components/project/project.detail.controller.js
@@ -6,6 +6,7 @@
         vm.title = 'projectDetailController';
         var ref = new Firebase("https://flickering-torch-5362.firebaseio.com/Project");
         vm.project = ProjectService.currentProject;
+        vm.ProjectFullName = vm.project.projectName + ' - ' + vm.project.customer;
 
         vm.newMember = function () {
             var templateUrl = '/app/components/project/project.addMember.html';
